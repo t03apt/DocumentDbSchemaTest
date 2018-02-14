01780befc576cbbcc9504b2d7c96e1ba7459cfaf
t03apt <t03apt@hotmail.com>
2018-02-15 00:44:20 +0100

    init

---
+++ DocumentDbSchema/DocumentCollectionContractResolver.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using Microsoft.Azure.Documents;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
+namespace DocumentDbSchema
+{
+    public class DocumentCollectionContractResolver : CamelCasePropertyNamesContractResolver
+    {
+        private IList<string> _propertiesToIgnore = new[] {
+            nameof(Resource.ResourceId),
+            nameof(Resource.ETag),
+            nameof(Resource.AltLink),
+            nameof(Resource.Timestamp),
+            nameof(Resource.SelfLink),
+            nameof(DocumentCollection.UniqueKeyPolicy),
+            nameof(DocumentCollection.DefaultTimeToLive)
+        };
+
+        private IList<string> _requiredProperties = new[] {
+            nameof(Resource.Id)
+        };
+
+        protected override IList<JsonProperty> CreateProperties(Type type, MemberSerialization memberSerialization)
+        {
+            var properties = base.CreateProperties(type, memberSerialization)
+                .Where(o => o.Writable)
+                .OrderBy(o => o.PropertyName);
+
+            return properties.ToList();
+        }
+
+        protected override JsonProperty CreateProperty(MemberInfo member, MemberSerialization memberSerialization)
+        {
+            if (!(member is PropertyInfo property) ||
+                !HasPublicGetter(property) ||
+                _propertiesToIgnore.Contains(property.Name)
+                )
+            {
+                return null;
+            }
+
+            var jsonProperty = base.CreateProperty(member, memberSerialization);
+
+            if (IsRequiredProperty(property))
+            {
+                jsonProperty.Required = Required.Always;
+            }
+            else if (jsonProperty.Required == Required.Default)
+            {
+                jsonProperty.Required = Required.DisallowNull;
+            }
+
+            IgnoreEmptyCollections(jsonProperty);
+
+            return jsonProperty;
+        }
+
+        private static void IgnoreEmptyCollections(JsonProperty jsonProperty)
+        {
+            if (typeof(ICollection).IsAssignableFrom(jsonProperty.PropertyType))
+            {
+                var oldShouldSerialize = jsonProperty.ShouldSerialize;
+                jsonProperty.ShouldSerialize = obj =>
+                {
+                    if (oldShouldSerialize != null && !oldShouldSerialize(obj))
+                    {
+                        return false;
+                    }
+
+                    if (!(jsonProperty.ValueProvider.GetValue(obj) is ICollection collection))
+                    {
+                        return false;
+                    }
+
+                    return collection == null || collection.Count != 0;
+                };
+            }
+        }
+
+        private static bool HasPublicGetter(PropertyInfo property)
+        {
+            return property.GetGetMethod() != null;
+        }
+
+        private static bool HasPublicSetter(PropertyInfo property)
+        {
+            return property.GetSetMethod() != null;
+        }
+
+        private bool IsRequiredProperty(PropertyInfo property)
+        {
+            return _requiredProperties.Contains(property.Name);
+        }
+    }
+}
---
+++ DocumentDbSchema/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using Microsoft.Azure.Documents;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json.Schema;
+using Newtonsoft.Json.Schema.Generation;
+
+namespace DocumentDbSchema
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            JSchemaGenerator generator = new JSchemaGenerator
+            {
+                ContractResolver = new DocumentCollectionContractResolver(),
+            };
+
+            generator.GenerationProviders.Add(new StringEnumGenerationProvider());
+
+            JSchema schema = generator.Generate(typeof(DocumentCollection));
+
+            Console.WriteLine(schema.ToString());
+
+            //File.WriteAllText(@"C:\temp\collection-schema.json", schema.ToString());
+
+            //TestSchema();
+        }
+
+        private static void TestSchema()
+        {
+            var serializerSettings = new JsonSerializerSettings
+            {
+                ContractResolver = new DocumentCollectionContractResolver()
+            };
+
+            var sample = GetSampleCollectionJson();
+            var collection = JsonConvert.DeserializeObject<DocumentCollection>(sample, serializerSettings);
+
+            var collectionAsJson = JsonConvert.SerializeObject(collection, serializerSettings);
+
+            //File.WriteAllText(@"C:\temp\sample.txt", $"{collectionAsJson}{Environment.NewLine}{sample}");
+
+            Console.WriteLine(collectionAsJson == sample);
+
+            string GetSampleCollectionJson()
+            {
+                var assembly = typeof(Program).Assembly;
+                var resourceName = $"{typeof(Program).Namespace}.sample-collection.json";
+
+                string data;
+
+                using (Stream stream = assembly.GetManifestResourceStream(resourceName))
+                using (StreamReader reader = new StreamReader(stream))
+                {
+                    data = reader.ReadToEnd();
+                }
+
+                var json = JObject.Parse(data);
+                SortProperties(json);
+
+                return json.ToString(Formatting.None);
+
+                void SortProperties(JObject jObj)
+                {
+                    var props = jObj.Properties().ToList();
+
+                    props.ForEach(o => o.Remove());
+
+                    props.OrderBy(p => p.Name).ToList().ForEach(jObj.Add);
+
+                    var values = props.Select(o => o.Value);
+
+                    var innerProps = values.OfType<JObject>()
+                        .Union(values.OfType<JArray>().SelectMany(o => o.Children()).OfType<JObject>())
+                        .ToList();
+
+                    innerProps.ForEach(SortProperties);
+                }
+            }
+        }
+    }
+}
